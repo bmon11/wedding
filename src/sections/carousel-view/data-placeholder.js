888d4fb6ac4592141194c9b379315fcbb19b3ee0
Tuvshuu <tuvshinbayar@cashone.mn>
2024-04-05 17:55:42 +0800

    main carousel

---
+++ src/sections/carousel-view/data-placeholder.js
@@ -0,0 +1,70 @@
+const dataPlaceholder = [
+  {
+    id: 0,
+    title: "Монголын шатрын аварга шалгаруулах тэмцээнд Хааны гамбит түрүүлэв",
+    coverUrl:
+      "https://images.pexels.com/photos/260024/pexels-photo-260024.jpeg?auto=compress&cs=tinysrgb&w=600",
+    description:
+      'In a thrilling display of strategic brilliance, the recent Mongolian Chess Championship witnessed the rise of a new chess prodigy, as Grandmaster Enkhbold "Khaan" Amarbold executed a series of daring moves that left spectators in awe',
+  },
+  {
+    id: 1,
+    title: "Талын Шатар: Монголд мартагдашгүй шатрын тэмцээн болж байна",
+    coverUrl:
+      "https://images.pexels.com/photos/4973821/pexels-photo-4973821.jpeg?auto=compress&cs=tinysrgb&w=600",
+    description:
+      "The Mongolian chess scene dazzled spectators as top contenders clashed in a fierce battle of wits. The tournament showcased brilliant strategic moves and unexpected victories, leaving chess enthusiasts on the edge of their seats.",
+  },
+  {
+    id: 2,
+    title:
+      "Ulan Bator Brilliance: Өсөн нэмэгдэж буй од Монголын шатрын титэмийг хожлоо",
+    coverUrl:
+      "https://images.pexels.com/photos/6598711/pexels-photo-6598711.jpeg?auto=compress&cs=tinysrgb&w=600",
+    description:
+      "A rising chess sensation from Ulan Bator took the spotlight at the Mongolian Chess Championship, captivating fans with a stellar performance. The young prodigy's strategic prowess and flawless execution secured a well-deserved victory, signaling a new era in Mongolian chess",
+  },
+  {
+    id: 3,
+    title:
+      "Хааны гамбит өрсөлдөгчдөө гайхшруулж байна: Монголд уламжлалт бус стратеги ялалт байгуулав",
+    coverUrl:
+      "https://images.pexels.com/photos/6598863/pexels-photo-6598863.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    description:
+      'In a surprising turn of events at the Mongolian Chess Championship, the daring "Khans Gambit" strategy emerged as the focal point. Spectators marveled at the unconventional moves that led to a triumphant outcome, establishing a fresh perspective in the realm of Mongolian chess',
+  },
+  {
+    id: 4,
+    title: "Шатрын сүр жавхлант Монгол дахь: Хаануудын ялалт",
+    coverUrl:
+      "https://images.pexels.com/photos/7104166/pexels-photo-7104166.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    description:
+      "Witness the grandeur of Mongolian chess as tactical mastery unfolds, crowning a new champion and captivating enthusiasts with strategic brilliance",
+  },
+  {
+    id: 5,
+    title: "Steppe Strategies: Unpredictable Wins at Mongolian Chess",
+    coverUrl:
+      "https://images.pexels.com/photos/5691872/pexels-photo-5691872.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    description:
+      "Mongolian chess reaches new heights with unpredictable strategies, showcasing the prowess of competitors who defy convention and secure surprising victories",
+  },
+  {
+    id: 6,
+    title: "Khan's Checkmate: A Pinnacle in Mongolian Chess",
+    coverUrl:
+      "https://images.pexels.com/photos/7978842/pexels-photo-7978842.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    description:
+      "The Mongolian Chess Championship witnesses the ascendancy of Khan's strategic finesse, culminating in a checkmate that echoes through the annals of Mongolian chess history",
+  },
+  {
+    id: 7,
+    title: "Nomadic Triumph: Dynamic Plays Dominate Mongolian Chess Arena",
+    coverUrl:
+      "https://images.pexels.com/photos/7104163/pexels-photo-7104163.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    description:
+      "Dynamic plays and nomadic-inspired strategies dominate the Mongolian chess arena, creating an electrifying atmosphere as players seize victory in an array of tactical duels",
+  },
+];
+
+export { dataPlaceholder };
--- src/sections/home/hero-item.js
+++ src/sections/home/hero-item.js
@@ -1,3 +1,9 @@
+import CarouselAnimation from "../carousel-view/carousel-animation";
+import { dataPlaceholder } from "../carousel-view/data-placeholder";
+
+// Placeholder
+
 export default function HeroItem(params) {
-  return <>"This is hero item"</>;
+  // return <>haha</>;
+  return <CarouselAnimation data={dataPlaceholder.slice(4, 8)} />;
 }
