febffdb718213ffd53d4e1a929026f8c35f04bfa
Tuvshinbayar <gtuvshin369@gmail.com>
2024-11-06 03:21:11 +0800

    temporarily deleted the contact section

--- src/layouts/main/header.js
+++ src/layouts/main/header.js
@@ -178,36 +178,11 @@ export default function Header() {
         },
       ],
     },
-    {
-      title: "ХОЛБОО БАРИХ",
-      path: "/contact",
-      caption: "",
-      // children: [
-      //   {
-      //     title: "Захиалга өгөх",
-      //     path: "#",
-      //     caption: "This is the caption",
-      //   },
-      //   {
-      //     title: "Санал хүсэлт",
-      //     path: "#",
-      //     // children: [
-      //     //   { title: "Page 2.1", path: "#" },
-      //     //   { title: "Page 2.2", path: "#" },
-      //     //   {
-      //     //     title: "Page 2.3",
-      //     //     path: "#",
-      //     //     children: [
-      //     //       { title: "Page 2.3.1", path: "#" },
-      //     //       { title: "Page 2.3.2", path: "#" },
-      //     //       { title: "Page 2.3.3", path: "#" },
-      //     //     ],
-      //     //   },
-      //     // ],
-      //   },
-      //   { title: "Page 3", path: "#" },
-      // ],
-    },
+    // {
+    //   title: "ХОЛБОО БАРИХ",
+    //   path: "/contact",
+    //   caption: "",
+    // },
   ];
 
   return (
